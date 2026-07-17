39ecdd8b18a9f5a899faf33d43c44179cbc86ee8
lamiazer10-blip <lamiazer10@gmail.com>
2026-07-17 08:37:53 +0100

    Add MainActivity for React Native integration

---
+++ android/app/android/app/src/main/java/com/retroengine/MainActivity.java
@@ -0,0 +1,27 @@
+package com.retroengine;
+
+import android.os.Bundle;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactActivityDelegate;
+
+public class MainActivity extends ReactActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+
+  @Override
+  protected String getMainComponentName() {
+    return "RetroEngine";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new DefaultReactActivityDelegate(
+        this,
+        getMainComponentName(),
+        DefaultNewArchitectureEntryPoint.getFabricDelegate());
+  }
+}
